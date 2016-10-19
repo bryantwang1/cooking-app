eca105e32105433a1c910e62ba2ae390dc26951b
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-19 13:02:53 -0700

    Complete functionality for all six conversions.

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,51 @@ $(function() {
     var gallon = parseInt($("input#gallon").val());
 
     result = gallonToLiter(gallon);
-    $(".output").text(gallon + " gallon(s) is/are " + result + " liters.")
+    $(".output").text(gallon + " gallon(s) is/are " + result + " liter(s).")
+  });
+
+  $("form#literToGallon").submit(function(event){
+    event.preventDefault();
+
+    var liter = parseInt($("input#liter1").val());
+
+    result = literToGallon(liter);
+    $(".output").text(liter + " liter(s) is/are " + result + " gallon(s).")
+  });
+
+  $("form#cupToLiter").submit(function(event){
+    event.preventDefault();
+
+    var cup = parseInt($("input#cup").val());
+
+    result = cupToLiter(cup);
+    $(".output").text(cup + " cup(s) is/are " + result + " liter(s).")
+  });
+
+  $("form#literToCup").submit(function(event){
+    event.preventDefault();
+
+    var liter = parseInt($("input#liter2").val());
+
+    result = literToCup(liter);
+    $(".output").text(liter + " liter(s) is/are " + result + " cup(s).")
+  });
+
+  $("form#ozToMl").submit(function(event){
+    event.preventDefault();
+
+    var floz = parseInt($("input#floz").val());
+
+    result = ozToMl(floz);
+    $(".output").text(floz + " fluid ounce(s) is/are " + result + " milliliter(s).")
+  });
+
+  $("form#mlToOz").submit(function(event){
+    event.preventDefault();
+
+    var milliliter = parseInt($("input#milliliter").val());
+
+    result = mlToOz(milliliter);
+    $(".output").text(milliliter + " milliliter(s) is/are " + result + " fluid ounce(s).")
   });
 });
